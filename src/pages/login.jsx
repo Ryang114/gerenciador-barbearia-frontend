e46a1f86a33bded8a97ce370528dce6cd0a588ed
ryang114 <rgomespec18@gmail.com>
2026-08-15 03:03:33 -0300

    começando a fazer a pagina de login

---
+++ src/pages/login.jsx
@@ -0,0 +1,13 @@
+import './login.css'
+
+function Login() {
+
+    return (
+        <div>
+                <h1 className="titulo">Login</h1>
+                <h2 className="subtitulo-login">Olá seja bem vindo! Por favor, faça seu login.</h2>
+        </div>
+    );
+}
+
+export default Login
