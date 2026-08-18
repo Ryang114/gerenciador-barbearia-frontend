016ba3b5ab40d8fcb565330c3021165ea40b374a
ryang114 <rgomespec18@gmail.com>
2026-08-17 22:52:21 -0300

    fazendo o forntend do login

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -3,16 +3,16 @@ import './login.css'
 function Login() {
 
     return (
-        <div>
-                <h1 className="titulo">Login</h1>
-                <h2 className="subtitulo-login">Olá seja bem vindo! Por favor, faça seu login.</h2>
-        </div>
+        <main>
+            <header>
+                <h1>Login</h1>
+                <h2>Olá, seja bem vindo porfavor faça o seu login</h2>
+            </header>
+            <form>
 
-        <form className= "formulorio-login">
-            <input type="email" placeholder= "Email"  />
-            <input type="password" placeholder="Senha" />
-            <button type="submit">Entrar</button>
-        </form>
+            </form>
+        </main>
+              
     );
 }
 
