fcb2c681b47237a705964325119629e5cc46df15
Ryan <ryanzin04113@gmail.com>
2026-08-31 09:54:27 -0300

    validando o front

--- src/pages/cadastro.jsx
+++ src/pages/cadastro.jsx
@@ -1,40 +1,44 @@
-import InputSenha from "../components/inputsenha"
-import Botao from "../components/Botao"
-import Footer from "../components/footer"
-import "./cadastro.css"
-import { useState } from "react"
-
-
+import InputSenha from "../components/inputsenha";
+import Botao from "../components/Botao";
+import Footer from "../components/footer";
+import "./cadastro.css";
+import { useState } from "react";
 
 function Cadastro() {
+  const [senha, setSenha] = useState("");
+  const [confirmarSenha, setConfirmarSenha] = useState("");
 
-    const [senha, setSenha] = useState("")
-    const [confirmarSenha, setConfirmarSenha] = useState("")
+  function Cadastrar() {
+    e.preventDefaut();
 
-    
+    if (senha == !confirmarSenha) console.log("As senhas não são iquais");
+    return;
+  }
 
-    return (
-        <main className="cadastro_container">
-            <header className="cadastro_cabecalho">
-                <h1 className="titulo_cadastro">Cadastro</h1>
-                <h2 className="subtitulo_cadastro">Por favor realize o cadastro caso não tenha uma senha</h2>
-            </header>
-            <form className="cadastro-form">
-                <span>Nome Completo:</span>
-                <input type="text" placeholder="Nome" />
-                <span>Email:</span>
-                <input type="email" placeholder="Email" />
-                <span>Telefone:</span>
-                <input type="tel" placeholder="Telefone" />
-                <span>Senha:</span>
-                <InputSenha />
-                <span>Corfirme sua senha:</span>
-                <InputSenha />
-                <Botao texto="Cadastrar" tipo="submit" />
-            </form>
-            <Footer />
-        </main>
-    )
+  console.log("As senhas são iguais");
+
+  return (
+    <main className="cadastro_container">
+      <header className="cadastro_cabecalho">
+        <h1 className="titulo_cadastro">Cadastro</h1>
+        <h2 className="subtitulo_cadastro">Por favor realize o cadastro caso não tenha uma senha</h2>
+      </header>
+      <form className="cadastro-form">
+        <span>Nome Completo:</span>
+        <input type="text" placeholder="Nome" />
+        <span>Email:</span>
+        <input type="email" placeholder="Email" />
+        <span>Telefone:</span>
+        <input type="tel" placeholder="Telefone" />
+        <span>Senha:</span>
+        <InputSenha valor={senha} aomudar={setSenha} />
+        <span>Corfirme sua senha:</span>
+        <InputSenha valor={confirmarSenha} aomudar={setConfirmarSenha} />
+        <Botao texto="Cadastrar" tipo="submit" aoClicar={Cadastrar}/>
+      </form>
+      <Footer />
+    </main>
+  );
 }
 
-export default Cadastro
+export default Cadastro;
